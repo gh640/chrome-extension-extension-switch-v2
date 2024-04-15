30e6f415fa869f3c846b5c536ba2b38bd0d7bfc8
Goto Hayato <gh640@hey.com>
2024-04-15 10:26:10 +0900

    feat: 🎸 Add options page

---
+++ options/script.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+import "./style.css";
+import {
+  get_extensions,
+  get_icon_url,
+  get_name_display,
+  get_excluded_extention_ids,
+  switch_excluded_status,
+} from "../utils.js";
+
+const NAME_MAX_SIZE = 30;
+
+const App = () => {
+  let [extensions, setExtensions] = useState([]);
+  let [excludedIds, setExcludedIds] = useState([]);
+
+  const refresh = async () => {
+    setExtensions(await get_extensions(true));
+    setExcludedIds(await get_excluded_extention_ids());
+  };
+
+  const handleClick = (extension, excludedIds) => {
+    return async (_event) => {
+      await switch_excluded_status(extension.id, excludedIds);
+      await refresh();
+    };
+  };
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  return (
+    <div>
+      <h1 class="heading">Excluded extensions</h1>
+      <div class="description">
+        Extensions checked here are excluded from the list of switches.
+      </div>
+      <div className="extensions">
+        {extensions.map((e) => (
+          <Item
+            extension={e}
+            excluded={excludedIds.includes(e.id)}
+            onClick={handleClick(e, excludedIds)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Item = ({ extension, excluded, onClick }) => {
+  const url = get_icon_url(extension);
+  const name = get_name_display(extension, NAME_MAX_SIZE);
+
+  return (
+    <label key={extension.name} className="extension">
+      <input type="checkbox" checked={excluded} onClick={onClick} />
+      {url ? (
+        <img
+          src={url}
+          alt={extension.name}
+          width="14"
+          height="14"
+          className="extension-image"
+        />
+      ) : null}
+      <span>{name}</span>
+    </label>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
